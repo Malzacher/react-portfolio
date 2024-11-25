1bc88e1bfc0000f21d13b4f7ac5368b10d714115
Nick <s3785693@student.rmit.edu.au>
2024-11-26 10:39:58 +1100

    Creating Switch case for displayed content

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,19 +1,42 @@
+import { useState } from "react";
+import WorkExperience from "./WorkExperience";
+import Portfolio from "./Portfolio";
+import Awards from "./Awards";
+import LiveWebBuilds from "./LiveWebBuilds";
+
 export default function Experience() {
-  function manageContent(buttonClick) {
-    console.log(buttonClick);
+  const [currentSection, setCurrentSection] = useState("Work Experience");
+
+  function renderContent() {
+    switch (currentSection) {
+      case "Work Experience":
+        return <WorkExperience />;
+      case "Portfolio":
+        return <Portfolio />;
+      case "Awards":
+        return <Awards />;
+      case "Live Web Builds":
+        return <LiveWebBuilds />;
+      default:
+        return <WorkExperience />;
+    }
   }
 
   return (
     <div className="experience-container">
       <div className="buttons">
-        <button onClick={() => manageContent("Work Experience")}>
+        <button onClick={() => setCurrentSection("Work Experience")}>
           Work Experience
         </button>
-        <button>Portfolio</button>
-        <button>Awards</button>
-        <button>Live Web Builds</button>
+        <button onClick={() => setCurrentSection("Portfolio")}>
+          Portfolio
+        </button>
+        <button onClick={() => setCurrentSection("Awards")}>Awards</button>
+        <button onClick={() => setCurrentSection("Live Web Builds")}>
+          Live Web Builds
+        </button>
       </div>
-      <div className="content-section"></div>
+      <div className="content-section">{renderContent()}</div>
     </div>
   );
 }
